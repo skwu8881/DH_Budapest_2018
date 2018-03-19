68606b3006c6f669b467eedccd973c12f2c73781
guagua0201 <tobyupu@gmail.com>
2018-03-19 22:03:15 +0800

    my first commit. in.txt - the raw data in2.txt - the data after using TF-IDF out.png - the temporary result graphing.cpp - do the graph TF_IDF.py - do TF_IDF

---
+++ TF_IDF.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 19 20:41:43 2018
+
+@author: fishtoby
+"""
+
+import jieba
+import sys
+
+from sklearn import feature_extraction
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+corpus = []
+with open('./in.txt', 'r') as f:
+    for line in f:
+        corpus.append(" ".join(jieba.cut(line.split(',')[0], cut_all=True)))
+
+vectorizer = TfidfVectorizer()
+tfidf = vectorizer.fit_transform(corpus)
+print(tfidf.shape)
+
+open('./in2.txt','w').close()
+words = vectorizer.get_feature_names()
+for i in range(len(corpus)):
+    for j in range(len(words)):
+        if tfidf[i,j] > 1e-5:
+            with open('./in2.txt','a') as nowF:
+              nowF.write(str(words[j]))
+              nowF.write(' ')
+              nowF.write(str(tfidf[i,j]))
+              nowF.write('\n')

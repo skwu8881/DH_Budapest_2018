62915bccdb61efdb3e8be7cc44b5f360d8d3d681
guagua0201 <tobyupu@gmail.com>
2018-03-21 16:53:06 +0800

    1. turn the tf-idf into c++ code 2. create new project 3. wordstTimes.txt contains the number of occurrences of each vocabulary 4. wordsWeight.txt contains the weight of each vocabulary in whole data base

---
+++ newProj/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "makeDataBase.hpp"
+using namespace std;
+int main() {
+	CDataBase db;
+	db.build();
+
+	return 0;
+}
---
+++ newProj/makeDataBase.hpp
@@ -0,0 +1,116 @@
+#ifndef __Make_DataBase__
+#define __Make_DataBase__
+
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <stdio.h>
+#include <math.h>
+#include <string>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+
+
+class CDataBase {
+private:
+	int m_fileNum = 100;
+	int wordsNum[100];
+	map<string, int> cnt[100], total;
+	vector< pair<double, string> > v;
+	inline string fresh(string s) {
+		string s2;
+		for (auto f : s) {
+			if (f >= 'A' && f <= 'Z') s2.push_back(f - 'A' + 'a');
+			else if (f >= 'a' && f <= 'z') s2.push_back(f);
+		}
+		return s2;
+	}
+	inline string i2s(int x) {
+		if (x == 0) return "0";
+		string s;
+		while (x) {
+			s.push_back(x % 10 + '0');
+			x /= 10;
+		}
+		reverse(s.begin(), s.end());
+		return s;
+	}
+	struct srt {
+		bool operator()(pair<double, string> const &a, pair<double, string> const &b) {
+			return a.first > b.first;
+		}
+	};
+public:
+	inline CDataBase() {
+		return;
+	}
+	inline ~CDataBase() {
+		return;
+	}
+	inline void build() {
+		string outName = ".\\wordsWeight.txt";
+		string outName2 = ".\\wordstTimes.txt";
+		fstream fout;
+		fout.open(outName,ios::out);
+		fout.close();
+		fout.open(outName2,ios::out);
+		fout.close();
+
+		for (int i = 1; i <= m_fileNum; i++) {
+			string fileName = ".\\data\\report" + i2s(i) + ".txt";
+			string s;
+			fstream fin;
+			fin.open(fileName.c_str(), ios::in);
+			while (fin >> s) {
+				s = fresh(s);
+				if (s.empty()) continue;
+				wordsNum[i]++;
+				total[s]++;
+				cnt[i][s]++;
+			}
+			fin.close();
+		}
+		fout.open(outName2.c_str(), ios::out | ios::app);
+		v.clear();
+		for (auto f : total) {
+			v.push_back(make_pair(f.second, f.first));
+		}
+		sort(v.begin(), v.end(),srt());
+		for (auto f : v) {
+			fout << f.second << ' ' << f.first << endl;
+		}
+		fout.close();
+		
+		fout.open(outName.c_str(), ios::out | ios::app);
+		v.clear();
+		for (auto f : total)
+			v.push_back(make_pair(f.second, f.first));
+		sort(v.begin(), v.end(), srt());
+		for (auto &f : v) {
+
+			int appearTime = 0;
+			for (int j = 1; j <= m_fileNum; j++)
+				if (cnt[j].find(f.second) != cnt[j].end()) appearTime++;
+
+			double tf, idf;
+			tf = (double)(f.first) / total.size();
+			idf = log((double)(m_fileNum) / appearTime);
+			f.first = tf*idf*sqrt(idf);
+
+		}
+		sort(v.begin(), v.end(), srt());
+		for (auto f : v) {
+			//cout << f.second << ' ' << f.first << endl;
+			fout << f.second << ' ' << f.first << endl;
+		}
+		fout.close();
+		return;
+	}
+
+};
+
+
+#endif

d1e048765f471b29f3ef1a2813ad3ba2af51b0e9
b0w1d <61a5t0@gmail.com>
2018-03-24 13:35:52 +0800

    Integrating Capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,57 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.10.1"
+
+set :application, "DH_Budapest_2018"
+set :local_repository, '/home/b0w1d/b0w1d/DH_Budapest_2018'
+set :rvm_ruby_version, '2.5.0@DH_Budapest_2018 --create'
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :assets_roles, [:app]
+set :migration_role, [:app]
+set :keep_releases, 3
+set :sidekiq_user, "ubuntu"
+set :sidekiq_role, [:app]
+set :pty,  false
+
+namespace :deploy do
+  task :restart do
+    on roles(:app), in: :sequence do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+  after :publishing, :restart
+  after 'deploy', 'deploy:migrate'
+end
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", "config/secrets.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure

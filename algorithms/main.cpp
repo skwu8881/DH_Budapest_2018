e1b871f602298ec878546884e3967b193d16c396
guagua0201 <tobyupu@gmail.com>
2018-04-11 04:56:28 +0800

    add graph.js

--- algorithms/main.cpp
+++ algorithms/main.cpp
@@ -5,10 +5,10 @@
 using namespace std;
 int main() {
 	CDataBase *db = new CDataBase;
-	// db->build();
+	//db->build();
 	CInputQuery *iq = new CInputQuery;
 	CJson *js = new CJson;
-	iq->readFile("algorithms/input.txt");
+	iq->readFile("./input.txt");
 	iq->askWeight();
 	js->makeJson();
 	return 0;
--- algorithms/makeJson.hpp
+++ algorithms/makeJson.hpp
@@ -13,6 +13,10 @@ using namespace std;
 
 class CJson {
 private:
+	typedef struct tagEDGE {
+		string source, target;
+		int val;
+	}EDGE;
 	typedef struct tagFEATURE {
 		double weight;
 		string word,type, cate;
@@ -39,12 +43,12 @@ private:
 		fstream fin;
 		string s;
 		double w;
-		fin.open("algorithms/resultWeight.txt", ios::in);
+		fin.open("./resultWeight.txt", ios::in);
 		while (fin >> s >> w) {
 			weight[s] = w;
 		}
 		fin.close();
-		fin.open("algorithms/NER.txt", ios::in);
+		fin.open("./NER.txt", ios::in);
 		while (getline(fin, s)) {
 
 			string nowNER,nowType,nowWord;
@@ -116,6 +120,78 @@ private:
 		}
 		fin.close();
 	}
+	map<pair<string,string>, int> num;
+	vector<EDGE> vEdge;
+	inline int word2Num(pair<string,string> s) {
+		if (num.find(s) != num.end()) return num[s];
+		else {
+			num[s] = num.size();
+			return num[s];
+		}
+	}
+	inline void addEdge(feature fa, feature fb) {
+		int a, b;
+		a = word2Num(make_pair(fa.word, fa.cate));
+		b = word2Num(make_pair(fb.word, fb.cate));
+		if (a == b) return;
+		EDGE e;
+		e.source = fa.word;
+		e.target = fb.word;
+		e.val = 1;
+		vEdge.push_back(e);
+	}
+	inline void findConnect() {
+		vector<feature> isPerson, isCate;
+		for (int i = 0; i < v.size(); i++) {
+			feature f = v[i];
+			if (f.word == "." || f.word == "?" || f.word == "!") {
+				if (isPerson.empty()) {
+					for (int i = 0; i < isCate.size(); i++) {
+						for (int j = i + 1; j < isCate.size(); j++) {
+							addEdge(isCate[i], isCate[j]);
+							addEdge(isCate[j], isCate[i]);
+						}
+					}
+				}
+				else {
+					for (auto p : isPerson) {
+						for (auto c : isCate) {
+							addEdge(p, c);
+						}
+					}
+					for (int i = 0; i < isPerson.size(); i++) {
+						for (int j = i + 1; j < isPerson.size(); j++) {
+							addEdge(isPerson[i], isPerson[j]);
+							addEdge(isPerson[j], isPerson[i]);
+						}
+					}
+				}
+				isPerson.clear();
+				isCate.clear();
+			}
+			else {
+				if (f.cate != "None") {
+					if (f.cate == "per") {
+						isPerson.push_back(f);
+					}
+					else {
+						isCate.push_back(f);
+					}
+				}
+			}
+		}
+	}
+	map<string, int> cateMap;
+	inline int class2Num(string s) {
+		if (s == "None") return 0;
+		else {
+			if (cateMap.find(s) != cateMap.end()) return cateMap[s];
+			else {
+				cateMap[s] = cateMap.size();
+				return cateMap[s];
+			}
+		}
+	}
 public:
 	inline CJson() {
 		return;
@@ -127,10 +203,14 @@ public:
 
 	inline void makeJson() {
 		readFile();
+		findConnect();
+		
+		// Json.txt
+
 		fstream fout;
-		fout.open("algorithms/Json.txt",ios::out);
+		fout.open("./Json.txt",ios::out);
 		fout.close();
-		fout.open("algorithms/Json.txt", ios::out | ios::app);
+		fout.open("./Json.txt", ios::out | ios::app);
 		fout << '['<<endl;
 		for (int i = 0; i < v.size();i++) {
 			feature f = v[i];
@@ -143,6 +223,33 @@ public:
 			fout << endl;
 		}
 		fout << ']';
+		fout.close();
+		
+
+		// graph.js
+
+		fstream fout;
+		fout.open("./graph.js", ios::out);
+		fout.close();
+		fout.open("./graph.js", ios::out | ios::app);
+		fout << "var graph = {" << endl;
+		fout << "  \"nodes\": [" << endl;
+		int cnt = 0;
+		for (auto f : num) {
+			fout << "    {\"id\": \"" << f.first.first << "\", \"group\" : " << class2Num(f.first.second)<<"}";
+			if (cnt < num.size() - 1) fout << ",";
+			fout << endl;
+			cnt++;
+		}
+		fout << "  ]," << endl << "  \"links\": [" << endl;
+		cnt = 0;
+		for (auto f : vEdge) {
+			fout << "    {\"source\": \"" << f.source << "\", \"target\" : \"" << f.target << "\", \"value\" : " << f.val << "}";
+			if (cnt < vEdge.size() - 1) fout << ",";
+			fout << endl;
+			cnt++;
+		}
+		fout << "  ]" << endl << "}";
 		fout.close();
 	}
 };
